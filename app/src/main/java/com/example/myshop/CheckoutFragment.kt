80d997e75349546e2db1580dc2694c1edb169d2d
BabbyBrown03 <revaldousskuncoroputra@gmail.com>
2025-09-16 14:11:21 +0700

    adjust

--- app/src/main/java/com/example/myshop/CheckoutFragment.kt
+++ app/src/main/java/com/example/myshop/CheckoutFragment.kt
@@ -8,17 +8,6 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.navArgs
 import com.example.myshop.databinding.FragmentCheckoutBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CheckoutFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CheckoutFragment : Fragment() {
 
     private var _binding : FragmentCheckoutBinding? = null
